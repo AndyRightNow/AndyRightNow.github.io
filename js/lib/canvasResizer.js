646fa10d042436bf9ae3722da7abae5d36447389
AndyRightNow <naruto950726@hotmail.com>
2016-08-16 21:10:10 +0800

    Stub out basic structure

--- js/lib/canvasResizer.js
+++ js/lib/canvasResizer.js
@@ -1,17 +1,31 @@
 /*
- * Resize the canvas to the size of the viewport
+ * Resize the canvas to the size of a certain percentage of the viewport
  */
 
 define(function(){
     return (function(){
         var canvasResizer = {
             _canvasList: [],
-            addResizer: function(canvasId) {
+            _sizes: {},
+            /*
+             * Add resize listener for the canvas with specific size
+             * Every time the browser resize, the canvas will change to its specific size
+             *
+             * @param {String} canvasId: The id of the canvas
+             * @param {Number} width, height: Non-negative number that is the percentage
+             *                                of the viewport. They are 1 by default.
+             */
+            addResizer: function(canvasId, width, height) {
+                width = width || 1;
+                height = height || 1;
+
                 var canvas = document.getElementById(canvasId);
 
                 if (canvas) {
-                    canvas.width = window.innerWidth;
-                    canvas.height = window.innerHeight;
+                    this._sizes[canvasId] = { 'width': width, 'height': height};
+
+                    canvas.width = width * window.innerWidth;
+                    canvas.height = height * window.innerHeight;
 
                     this._canvasList.push(canvas);
                 }
@@ -20,8 +34,8 @@ define(function(){
 
         window.addEventListener('resize', function(e){
             canvasResizer._canvasList.forEach(function(ele){
-                ele.width = window.innerWidth;
-                ele.height = window.innerHeight;
+                ele.width = window.innerWidth * canvasResizer._sizes[ele.id].width;
+                ele.height = window.innerHeight * canvasResizer._sizes[ele.id].height;
             });
         });
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-require(['jquery', 'RandomBackgroundGenerator', 'canvasResizer'],
-        function($, RandomBackgroundGenerator, canvasResizer){
+require(['jquery', 'RandomBackgroundGenerator', 'canvasResizer', 'scroll_link'],
+function($, RandomBackgroundGenerator, canvasResizer, youarenotgonnausethis){
+    var CANVAS_ID = 'canvas';
+    var canvas = document.getElementById(CANVAS_ID);
+
+    //  Resize the canvas automatically by 100% viewport width and height
+    canvasResizer.addResizer(CANVAS_ID, 1, 1);
+
+    //----------------------------------
+    //  Click to generate random background
+    //----------------------------------
+    var background = new RandomBackgroundGenerator(CANVAS_ID, 'Polygonal',
+        '#4183d7', '#26A65B', '#663399');
+    background.getMode().setDensity(0.8);
+    background.generate();
+    background.enabled = true;
+    document.addEventListener('click', function(event){
+        if (event.target.tagName === 'SECTION' && background.enabled) {
+            background.generate();
+        }
+    });
 });
